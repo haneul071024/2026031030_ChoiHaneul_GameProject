47fb9e930491c08846f1c7a25a75596747e577c0
moonstone <a01039162341@gmail.com>
2026-09-02 14:52:13 +0900

    Game library

--- finalshooting/Program.cs
+++ finalshooting/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace finalshooting
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    //internal class Program
+   //{
+    //    //static void Main(string[] args)
+    //    //{
+    //    //    Console.WriteLine("Hello, World!");
+    //    //}
+    //}
 }
